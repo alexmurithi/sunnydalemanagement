c88705509e894c0e3070fcf460d11dee108da143
Alex Murithi <gitsaleks@gmail.com>
2021-07-07 20:30:36 +0100

    team card completion

--- src/Components/LandingPage/Team.js
+++ src/Components/LandingPage/Team.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import {Box, Container,Typography,Card,CardContent,Grid} from '@material-ui/core';
+import {Box, Container,Typography,Card,CardContent,Grid,IconButton} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
 import YouTube from "../../Assets/Images/Youtube.png";
 import SimbaImg from "../../Assets/Images/simba.jpg";
 import ZebraImg from "../../Assets/Images/zebra.jpg";
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import MailIcon from '@material-ui/icons/Mail';
 
 const TeamStyles =makeStyles((theme)=>({
     root:{
@@ -12,10 +15,14 @@ const TeamStyles =makeStyles((theme)=>({
     },
     card1:{
       backgroundColor:theme.palette.secondary.main,
-
+      borderRadius:"36px"
+    },
+    card2:{
+        borderRadius:"36px"
     },
     card3:{
-        backgroundColor:theme.palette.primary.main
+        backgroundColor:theme.palette.primary.main,
+        borderRadius:"36px"
     },
     cardContent:{
         padding:theme.spacing(8)
@@ -31,6 +38,20 @@ const TeamStyles =makeStyles((theme)=>({
             objectFit:"cover"
         }
     },
+    cardTeamName:{
+        textAlign:"center",
+        paddingBottom:theme.spacing(2)
+    },
+    cardTeamPosition:{
+        textAlign:"center",
+        color:theme.palette.white,
+        paddingBottom:theme.spacing(2)
+
+    },
+    teamCenterPosition:{
+        color:"#0093dd",
+        paddingBottom:theme.spacing(2),
+    }
 
 }))
 
@@ -57,7 +78,32 @@ const Team =() =>{
 
                                 <Card classes={{root:classes.card1}}>
                                     <CardContent classes={{root:classes.cardContent}}>
-                                        <Typography variant="h5" align={"center"}>Ambrose Mugambi</Typography>
+
+                                        <Typography variant="h5" className={classes.cardTeamName}>
+                                            Ambrose Mugambi
+                                        </Typography>
+
+                                        <Typography variant="h5" className={classes.cardTeamPosition}>
+                                            Founder & CEO
+                                        </Typography>
+
+                                        <Grid container>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <TwitterIcon  fontSize="large" style={{color:"#ffffff"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <FacebookIcon fontSize="large" style={{color:"#ffffff"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <MailIcon fontSize="large" style={{color:"#ffffff"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                        </Grid>
                                     </CardContent>
                                 </Card>
                             </Box>
@@ -69,8 +115,37 @@ const Team =() =>{
                                 </div>
 
                                 <Card classes={{root:classes.card2}}>
-                                    <CardContent>
-                                        <Typography variant="h5" align={"center"}>John Doe</Typography>
+                                    <CardContent classes={{root:classes.cardContent}}>
+                                        <Typography variant="h5" className={classes.cardTeamName}>
+                                            John Doe
+                                        </Typography>
+
+                                        <Typography
+                                            variant="h5"
+                                            align={"center"}
+                                            className={classes.teamCenterPosition}
+                                        >
+                                            Founder & CEO
+                                        </Typography>
+
+                                        <Grid container>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <TwitterIcon  fontSize="large" style={{color:"#0093dd"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <FacebookIcon fontSize="large" style={{color:"#0093dd"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <MailIcon fontSize="large" style={{color:"#0093dd"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                        </Grid>
+
                                     </CardContent>
                                 </Card>
                             </Box>
@@ -82,8 +157,34 @@ const Team =() =>{
                                 </div>
 
                                 <Card classes={{root:classes.card3}}>
-                                    <CardContent>
-                                        <Typography variant="h5" align={"center"}>James Kim</Typography>
+                                    <CardContent classes={{root:classes.cardContent}}>
+
+                                        <Typography variant="h5" className={classes.cardTeamName}>
+                                            James Kim
+                                        </Typography>
+
+                                        <Typography variant="h5" align={"center"} className={classes.cardTeamPosition}>
+                                            Founder & CEO
+                                        </Typography>
+
+                                        <Grid container>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <TwitterIcon  fontSize="large" style={{color:"#ffffff"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <FacebookIcon fontSize="large" style={{color:"#ffffff"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                            <Grid item lg={4}>
+                                                <IconButton>
+                                                    <MailIcon fontSize="large" style={{color:"#ffffff"}}/>
+                                                </IconButton>
+                                            </Grid>
+                                        </Grid>
+
                                     </CardContent>
                                 </Card>
                             </Box>
